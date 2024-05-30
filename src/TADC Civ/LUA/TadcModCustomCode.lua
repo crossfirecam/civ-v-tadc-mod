5eee7ef187e1a426aa0309ed3fb38eb291883835
CrossfireCam <30458404+crossfirecam@users.noreply.github.com>
2024-05-30 23:43:12 +1000

    Pomni Civ: Gangle's Theater Done
    - Wow that was quick. Gangle's Theater functionality is working. Half the time we earn a Comedy Theater, half the time we earn a Tragedy Theater.
    - Altered text for Acrobat

--- src/TADC Civ/LUA/TadcModCustomCode.lua
+++ src/TADC Civ/LUA/TadcModCustomCode.lua
@@ -7,6 +7,9 @@
 --------------------------------------------------------------
 
 
+--
+-- TADC1: Cainic Empire
+--
 function Tadc1DigitalCircusAbility(iPlayer, iCity, iBuilding)
 	-- Each time a construction finishes, check if it's a TADC_CIRCUS and run randomspawn
 	local iTadcCircus = GameInfoTypes["BUILDING_TADC_CIRCUS"];
@@ -62,16 +65,16 @@ function Tadc1DigitalCircusAbility(iPlayer, iCity, iBuilding)
 		local chosenUnit2 = eligibleUnits[math.random(#eligibleUnits)];
 		local pUnit2 = pPlayer:InitUnit(GameInfoTypes[chosenUnit2], pCity:GetX(), pCity:GetY());
 		
+		-- Show notification
 		local cityName = pCity:GetName();
-		local text = cityName .. "'s newly built Digital Circus has welcomed two NPCs to join the fight!";
 		local heading = "Units summoned by " .. cityName .. "'s new Digital Circus";
+		local text = cityName .. "'s newly built Digital Circus has welcomed two NPCs to join the fight!";
      	pPlayer:AddNotification(NotificationTypes.NOTIFICATION_GENERIC, text, heading);
 		print("-- Tadc1DigitalCircusAbility: Units spawned successfully");
 	end
 end
 
 function insertUnitIfResearched(pPlayer, techName, unitName)
-
 	pTeam = Teams[pPlayer:GetTeam()];
 	-- If a player has researched a certain tech, then add it as an elibible unit for the Digital Circus ability
 	if pTeam:IsHasTech(GameInfoTypes[techName]) then
@@ -83,9 +86,54 @@ function insertUnitIfResearched(pPlayer, techName, unitName)
 end
 
 
+
+--
+-- TADC2: Cast-Members Alliance
+--
+function Tadc2GanglesTheaterAbility(iPlayer, iCity, iBuilding)
+	-- Each time a construction finishes, check if it's a BUILDING_TADC_GANGLES_THEATER_PLACEHOLDER and run replacement code
+	local iTadcGangleTheater = GameInfoTypes["BUILDING_TADC_GANGLES_THEATER_PLACEHOLDER"];
+	local iTadcGangleTheaterAlt1 = GameInfoTypes["BUILDING_TADC_GANGLES_THEATER_COMEDY"];
+	local iTadcGangleTheaterAlt2 = GameInfoTypes["BUILDING_TADC_GANGLES_THEATER_TRAGEDY"];
+	local pPlayer = Players[iPlayer];
+	if (iBuilding == iTadcGangleTheater) then
+		print("-- Tadc2GanglesTheaterAbility: A player has just built Gangle's Theater Placeholder.");
+		local pCity = pPlayer:GetCityByID(iCity)
+		pCity:SetNumRealBuilding(iTadcGangleTheater, 0);
+		
+		local cityName = pCity:GetName();
+		local heading;
+		local text;
+
+		if (math.random() < 0.5) then
+			pCity:SetNumRealBuilding(iTadcGangleTheaterAlt1, 1);
+			heading = cityName .. "'s new Comedy Theater is complete!";
+			text = cityName .. "'s newly built Comedy Theater provides an extra +1 Happiness!";
+		else
+			pCity:SetNumRealBuilding(iTadcGangleTheaterAlt2, 1);
+			heading = cityName .. "'s new Tragedy Theater is complete!";
+			text = cityName .. "'s newly built Tragedy Theater provides an extra +1 Culture!";
+		end
+
+		-- Show notification
+     	pPlayer:AddNotification(NotificationTypes.NOTIFICATION_GENERIC, text, heading);
+		print("-- Tadc2GanglesTheaterAbility: Building replaced successfully");
+	end
+end
+
+
+--
+-- Generic Functions
+--
 function IsValidPlayer(player)
 	return player ~= nil and player:IsAlive() and not player:IsBarbarian()
 end
 
+
+
+--
+-- Start Main Code
+--
 print("TADC Lua loaded successfully")
 GameEvents.CityConstructed.Add( Tadc1DigitalCircusAbility );
+GameEvents.CityConstructed.Add( Tadc2GanglesTheaterAbility );
